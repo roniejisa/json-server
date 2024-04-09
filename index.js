3798219e5619aabf172badb1f3a91e6a534f853b
Phạm Minh Hiếu <hieupm248@gmail.com>
2024-04-09 23:16:27 +0700

    update code

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const router = jsonServer.router("./db.json");
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
