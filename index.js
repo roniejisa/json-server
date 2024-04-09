77cedd70002ccb93f8da1878b800a0811a18d364
Phạm Minh Hiếu <hieupm248@gmail.com>
2024-04-09 23:03:11 +0700

    update code

---
+++ index.js
@@ -0,0 +1,22 @@
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+    // Add custom route here if needed
+    jsonServer.rewriter({
+        "/*": "/$1",
+    })
+);
+server.use(router);
+server.listen(3000, () => {
+    console.log("JSON Server is running");
+});
+
+module.exports = server;
