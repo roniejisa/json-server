333222dcfc37e9d10a921ef00784e1066ffccbeb
Phạm Minh Hiếu <hieupm248@gmail.com>
2024-04-09 23:39:30 +0700

    update code

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const fs = require("fs");
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+
+const router = jsonServer.router(db);
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 server.use(middlewares);
